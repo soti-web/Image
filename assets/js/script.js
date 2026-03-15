324a4762461ec23a471db3ca410c2b0e34d5e7e2
soti-web <soti03e250058@epu.edu.iq>
2026-03-15 06:51:40 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,13 +140,6 @@ for (let i = 0; i < formInputs.length; i++) {
 
 
 
-// SEND MESSAGE DUGMA
-const form = document.querySelector(".form");
-const successMsg = document.getElementById("successMsg");
-
-form.addEventListener("submit", function() {
-  successMsg.style.display = "block";
-});
 
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
